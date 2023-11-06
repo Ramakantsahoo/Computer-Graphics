03ad251d4d52f6bc89a777bc6cb4d1bee515f3cc
Ramakant <98227695+Ramakantsahoo@users.noreply.github.com>
2023-11-06 23:21:27 +0530

    Add files via upload

---
+++ POLYGONF.C
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <conio.h>
+#include <graphics.h>
+#include <dos.h>
+
+void polyfill8(int x, int y, int fc, int bc)
+{
+	if (getpixel(x, y) != bc && getpixel(x, y) != fc)
+	{
+		putpixel(x, y, fc);
+		polyfill8(x + 1, y, fc, bc);
+		polyfill8(x+1, y+1, fc, bc);
+		polyfill8(x, y + 1, fc, bc);
+		polyfill8(x-1, y+1, fc, bc);
+		polyfill8(x - 1, y, fc, bc);
+		polyfill8(x-1, y-1, fc, bc);
+		polyfill8(x, y - 1, fc, bc);
+		polyfill8(x+1, y-1, fc, bc);
+	}
+}
+
+int main()
+{
+	int x = 100, y = 100, r = 15;
+	int gd = DETECT, gm;
+	initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
+	rectangle(50, 50, 100, 100);
+	polyfill8(55, 55, 4, 15);
+	delay(5000);
+	getch();
+	closegraph();
+	return 0;
+}
